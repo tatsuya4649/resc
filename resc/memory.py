e467f0ad15a20e9cb2e1c777b56c7638107fcc9b
tatsuya <tatsuya4649developer@gmail.com>
2021-07-21 16:11:27 +0900

    MemoryDetect added

--- resc/memory.py
+++ resc/memory.py
@@ -1,10 +1,64 @@
 import psutil
+from enum import Enum
 
-def memory_percent():
-	return psutil.virtual_memory().percent
+class MemoryTypeError(TypeError):
+	pass
+class MemoryValueError(ValueError):
+	pass
 
-def memory_used():
-	return psutil.virtual_memory().used
+class MemoryDetectMode(Enum):
+	PERCENT="percent"
+	USED="used"
+	AVAILABLE="available"
 
-def memory_available():
-	return psutil.virtual_memory().available
+class MemoryDetect:
+	"""
+	"""
+	def __init__(
+		self,
+		threshold,
+		mode=MemoryDetectMode.PERCENT.value,
+	):
+		if not isinstance(threshold,int) and not isinstance(threshold,float):
+			raise MemoryTypeError("threshold must be int or float type.")
+		self._threshold = threshold
+
+		if not isinstance(mode,str):
+			raise MemoryTypeError("mode most be string type.")
+		if mode not in [x.value for x in MemoryDetectMode if x.value == mode]:
+			raise MemoryValueError(f"{mode} is invalid. valid value: {[x.value] for x in MemoryDetectMode}")
+		self._mode = [x for x in MemoryDetectMode if x.value == mode][0]
+	
+	@property
+	def mode(self):
+		return self._mode
+
+	@property
+	def check(self):
+		"""
+		Check over Memory threshold.
+		over threshold: return False
+		within threshold: return True
+                """
+		res = eval(f"self.{self._mode.value}()")
+		if res is None:
+			raise MemoryValueError("memory value must be not None.")
+		if res < self._threshold:
+			return True
+		else:
+			return False
+	
+	@property
+	def threshold(self):
+		return self._threshold
+	@staticmethod
+	def percent():
+		return psutil.virtual_memory().percent
+
+	@staticmethod
+	def used():
+		return psutil.virtual_memory().used
+
+	@staticmethod
+	def available():
+		return psutil.virtual_memory().available
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -3,26 +3,32 @@ from resc.memory import *
 
 class TestMemory(unittest.TestCase):
 	def test_memory(self):
-		res = memory_percent()
+		memory = MemoryDetect(80)
+		res = memory.percent()
 		self.assertIsNotNone(res)
 		self.assertIsInstance(res,float)
 		self.assertGreaterEqual(res,0.0)
 
 		print(f'MEMORY USAGE PERCENT {res}% ({type(res)})')
 
-		res = memory_used()
+		res = memory.used()
 		self.assertIsNotNone(res)
 		self.assertIsInstance(res,int)
 		self.assertGreaterEqual(res,0)
 
 		print(f'MEMORY USED {res}bytes [ {round(res/(1024*1024),1)}MB , {round(res/(1024*1024*1024),1)}GB ] ({type(res)})')
 
-		res = memory_available()
+		res = memory.available()
 		self.assertIsNotNone(res)
 		self.assertIsInstance(res,int)
 		self.assertGreaterEqual(res,0)
 
 		print(f'MEMORY AVAILABLE {res}bytes [ {round(res/(1024*1024),1)}MB , {round(res/(1024*1024*1024),1)}GB ] ({type(res)})')
+
+		res = memory.check
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,bool)
+		print(f'MEMORY CHECK {res} (type(res))')
 
 
 if __name__ == "__main__":
