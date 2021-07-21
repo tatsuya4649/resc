91c4a24a4e1d489454a16f9b4ddb1754f986843a
tatsuya <tatsuya4649developer@gmail.com>
2021-07-21 15:39:35 +0900

    CPUDetect added

--- resc/cpu.py
+++ resc/cpu.py
@@ -1,14 +1,72 @@
 import psutil
+from enum import Enum
 
-def ave_cpu_percent(interval):
-	if not isinstance(interval,float) and not isinstance(interval,int):
-		raise TypeError("interval must by float or int type.")
-	return psutil.cpu_percent(interval=float(interval),percpu=False)
+class CPUTypeError(TypeError):
+	pass
+class CPUValueError(ValueError):
+	pass
 
-def cpu_percent(interval):
-	if not isinstance(interval,float) and not isinstance(interval,int):
-		raise TypeError("interval must by float or int type.")
-	return psutil.cpu_percent(interval=float(interval),percpu=True)
+class CPUDetectMode(Enum):
+	PERCENT="percent"
+	LOADAVG="loadavg"
 
-def cpuloadavg():
-	return psutil.getloadavg()
+class CPUDetect:
+	"""
+	Detect CPU threshold by percent(%) or load average.
+	"""
+	_CPU_INTERVAL_DEFAULT=10
+	def __init__(
+		self,
+		threshold,mode=CPUDetectMode.PERCENT.value,
+		interval=_CPU_INTERVAL_DEFAULT,
+	):
+		if not isinstance(threshold,int) and not isinstance(threshold,float):
+			raise CPUTypeError("threshold must be int or float type.")
+		self._threshold = threshold
+
+		if not isinstance(mode,str):
+			raise CPUTypeError(f"mode must be string type.({[x.value for x in CPUDetectMode]})")
+		if mode not in [x.value for x in CPUDetectMode]:
+			raise CPUValueError(f"{mode} is invalid.(valid value:{[x.value for x in CPUDetectMode]})")
+		self._mode = [ x for x in CPUDetectMode if x.value == mode][0]
+
+		if not isinstance(interval,int) and not isinstance(interval,float):
+			raise CPUTypeError("interval must be int or float type.")
+		self._interval = interval
+
+	@property
+	def mode(self):
+		return self._mode
+
+	@property
+	def check(self):
+		"""
+		Check over CPU threshold.
+		over threshold: return False
+		within threshold: return True
+		"""
+		res = eval(f"self.{self._mode.value}({self._interval})")
+		if res < self.threshold:
+			return True
+		else:
+			return False
+
+	@property
+	def threshold(self):
+		return self._threshold
+
+	@staticmethod
+	def percent(interval):
+		if not isinstance(interval,float) and not isinstance(interval,int):
+			raise TypeError("interval must by float or int type.")
+		return psutil.cpu_percent(interval=float(interval),percpu=False)
+
+	@staticmethod
+	def percent_percpu(interval):
+		if not isinstance(interval,float) and not isinstance(interval,int):
+			raise TypeError("interval must by float or int type.")
+		return psutil.cpu_percent(interval=float(interval),percpu=True)
+
+	@staticmethod
+	def loadavg():
+		return psutil.getloadavg()
--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -2,16 +2,22 @@ import unittest
 from resc.cpu import *
 
 _CPU_WAIT_TIME = 1
+_CPU_MODE = "percent"
 
 class TestCPU(unittest.TestCase):
 	def test_cpu(self):
-		res = ave_cpu_percent(_CPU_WAIT_TIME)
+		cpu = CPUDetect(
+			80,
+			mode=_CPU_MODE,
+			interval=_CPU_WAIT_TIME
+		)
+		res = cpu.percent(_CPU_WAIT_TIME,)
 		self.assertIsNotNone(res)
 		self.assertIsInstance(res,float)
-		print(f'CPU AVERAGE RATIO: {res}%(float)')
+		print(f'CPU AVERAGE RATIO: {res}%({type(res)})')
 		self.assertGreaterEqual(res,0.0)
 
-		res = cpu_percent(_CPU_WAIT_TIME)
+		res = cpu.percent_percpu(_CPU_WAIT_TIME)
 		self.assertIsNotNone(res)
 		self.assertIsInstance(res,list)
 		print(f'CPU RATIO PER CPU: (list of float)')
@@ -20,7 +26,7 @@ class TestCPU(unittest.TestCase):
 			self.assertIsInstance(res[cpu_number],float)
 			self.assertGreaterEqual(res[cpu_number],0.0)
 
-		res = cpuloadavg()
+		res = cpu.loadavg()
 		self.assertIsNotNone(res)
 		self.assertIsInstance(res,tuple)
 		print(f'CPU LOAD AVG PER CPU: (list of float)')
@@ -28,6 +34,11 @@ class TestCPU(unittest.TestCase):
 			print(f'CPU LOAD AVG[{cpu_number}]: {res[cpu_number]}({type(res[cpu_number])})')
 			self.assertIsInstance(res[cpu_number],float)
 			self.assertGreaterEqual(res[cpu_number],0.0)
+
+		res = cpu.check
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,bool)
+		print(f'CPU THRESHOLD CHECK: {res} (type(res))')
 
 
 if __name__ == "__main__":
