3e569c67631a745424af43aef48ba60c7232d44d
tatsuya <tatsuya4649developer@gmail.com>
2021-07-22 21:56:30 +0900

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+def _requires_from_file(filename):
+	with open(filename,"r") as f:
+		return f.read().splitlines()
+
+setup(
+	install_requires=_requires_from_file("requirements.txt"),
+)
--- tests/test_resc.py
+++ tests/test_resc.py
@@ -5,31 +5,31 @@ from resc._resc import Resc
 _INTERVAL=0
 
 class TestResc(unittest.TestCase):
-#	def test_resc(self):
-#		resc = Resc(
-#			cpu={"threshold":80,"interval":_INTERVAL},
-#			memory={"threshold":80},
-#			disk={"threshold":80,"path":"/"},
-#		)
-#		res = resc.thresholds
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,dict)
-#		print(f'RESC THRESHOLDS {res}')
-#
-#		res = resc.checks
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,dict)
-#		print(f'RESC CHECKS {res}')
-#
-#		res = resc.over_one
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,bool)
-#		print(f'RESC OVER ONE {res}')
-#
-#		res = resc.overs
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,list)
-#		print(f'RESC OVERS {res}')
+	def test_resc(self):
+		resc = Resc(
+			cpu={"threshold":80,"interval":_INTERVAL},
+			memory={"threshold":80},
+			disk={"threshold":80,"path":"/"},
+		)
+		res = resc.thresholds
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,dict)
+		print(f'RESC THRESHOLDS {res}')
+
+		res = resc.checks
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,dict)
+		print(f'RESC CHECKS {res}')
+
+		res = resc.over_one
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,bool)
+		print(f'RESC OVER ONE {res}')
+
+		res = resc.overs
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,list)
+		print(f'RESC OVERS {res}')
 
 	def test_register(self):
 		resc = Resc(
@@ -46,9 +46,11 @@ class TestResc(unittest.TestCase):
 		@resc.register("*/1 * * * *")
 		def world(a,b):
 			print("hello world")
+			import time
 			print(a,b)
+			print(time.time())
 
-		world(1,"hello",b="resc test script")
+		world(1,b="resc test script")
 #		hello()
 if __name__ == "__main__":
 	unittest.main()
