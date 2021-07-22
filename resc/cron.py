689b4af93acdfd139c4e971d9320bb46f0ea1296
tatsuya <tatsuya4649developer@gmail.com>
2021-07-22 11:07:13 +0900

    cron operation class

--- resc/cron.py
+++ resc/cron.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import re
+import subprocess
 
 class CronScale(Enum):
 	MIN={"name":"minute","min":0,"max":59}
@@ -21,6 +22,9 @@ class CronRegularExpressionError(ValueError):
 	pass
 
 class Cron:
+	"""
+	"""
+	_CRON_RE = r'(([0-6]?[0-9]-[0-6]?[0-9])|((,?[0-6]?[0-9])+)|(\*))(/[0-6]?[0-9])?'
 	def __init__(
 		self,
 		command,
@@ -29,99 +33,25 @@ class Cron:
 		if not isinstance(command,str):
 			raise CronTypeError("command must be str type.")
 		self._command = command
+		self._command = self._quote_replace()
 
 		if interval_str is None:
 			raise CronValueError("interval_str must be not None.")
-#		if interval_str is not None and intervals is not None:
-#			raise CronValueError("intervals or interval_str either one must be None.")
 
-		if interval_str is not None:
-			self._interval_str = interval_str
-#			self._intervals = None
-			self._str_to_lists()
-			return
+		self._interval_str = interval_str
+		self._str_to_lists()
+		self._totalline = f"{self._interval_str} \"{self._command}\"\n"
 
-#		self._interval_str = None
-#		if not isinstance(intervals,list):
-#			raise CronTypeError("intervals must be list type.")
-#		if len(intervals) == 0:
-#			raise CronValueError("intervals must have one or more interval elem")
-#
-#
-#		self._intervals = list()
-#		for elem in intervals:
-#			if not isinstance(elem,dict):
-#				raise CronTypeError("intervals element must be dict type.")
-#			if "scale" not in elem.keys() or "interval" not in elem.keys() or "mode" not in elem.keys():
-#				raise CronValueError("interval element type must have \"scale\",\"interval\",\"mode\" keys.")
-#			
-#			interval = elem["interval"]
-#			interval_mode = elem["mode"]
-#			interval_scale = elem["scale"]
-#
-#			_crdict = dict()
-#			if not isinstance(interval,int):
-#				raise CronTypeError("interval must be int type.")
-##			self._interval = interval
-#			_crdict["interval"] = interval
-#			if not isinstance(interval_mode,str):
-#				raise CronTypeError("interval_mode must be str type.")
-#
-#			if len([x.value['name'] for x in CronScale if re.match(rf'^{x.value["name"]}$',f'{interval_scale}',flags=re.IGNORECASE) is not None]) == 0:
-#				raise CronValueError(f"interval_scale is invalid value. valid value ({[x.value['name'] for x in CronScale]})")
-##			self._interval_scale = [x for x in CronScale if re.match(rf'^{x.value["name"]}$',f'{interval_scale}',flags=re.IGNORECASE) is not None][0]
-#			_crdict["scale"] = [x for x in CronScale if re.match(rf'^{x.value["name"]}$',f'{interval_scale}',flags=re.IGNORECASE) is not None][0]
-#
-#			if len([x.value for x in CronMode if re.match(rf'^{x.value}$',f'{interval_mode}',flags=re.IGNORECASE) is not None]) == 0:
-#				raise CronValueError(f"interval_mode is invalid value. valid value ({[x.value for x in CronMode]})")
-#			_crdict["mode"] = [x for x in CronMode if re.match(rf'^{x.value}$',f'{interval_mode}',flags=re.IGNORECASE) is not None][0]
-##			self._interval_mode = [x for x in CronMode if re.match(rf'^{x.value}$',f'{interval_mode}',flags=re.IGNORECASE) is not None][0]
-#			
-#			self._intervals.append(_crdict)
-#			self._lists_to_str()
-#	@property
-#	def intervals(self):
-#		return self._intervals
 	@property
 	def interval_str(self):
 		return self._interval_str
-
-#	def _lists_to_str(self):
-#		if not hasattr(self,"_intervals") or self._intervals is None or len(self._intervals)==0:
-#			raise CronAttributeError("_intervals must be not None.")
-#		self._interval_str = str()
-#		for scale in list(CronScale):
-#			scales = [x for x in self._intervals if x["scale"] is scale]
-#			if len(scales) == 0:
-#				self._interval_str += "* "
-#				continue
-#			before = [x for x in scales if x["mode"] == CronMode.SCHEDULE]
-#			if len(before) == 0:
-#				self._interval_str += f"*"
-#			else:
-#				if len(x for x in before if isinstance(x["interval"],str)) > 0:
-#					raise CronValueError("cron time interval is invalid valid.")
-#				for schedule in before:
-#					if isinstance(schedule["interval"],str) and schedule["interval"] == "*":
-#						self._interval_str += f"*"
-#						break
-#					self._interval_str += f"{schedule["interval"]}"
-#				self._interval_str.rstrip(",")
-#
-#			after = [x for x in scales if x["mode"] == CronMode.INTERVAL]
-#			if len(after) > 0:
-#				if len(after) > 1:
-#					raise CronValueError(f"interval mode of {scale.value["name"]} must have only one.")
-#				self._interval_str += f"/{after["interval"] if isinstance(after["interval"],int) else "*"}"
-#			self._interval_str += " "
-#			
-#		self._interval_str = self._interval_str.rstrip(' ')
-#		print(self._interval_str)
-
+	@property
+	def totalline(self):
+		return self._totalline
 
 	def _str_to_lists(self):
 		self._intervals = list()
-		template = r'(([0-6]?[0-9]-[0-6]?[0-9])|((,?[0-6]?[0-9])+)|(\*))(/[0-6]?[0-9])?'
+		template = self._CRON_RE
 
 		results = re.finditer(template,self._interval_str)
 		results = [i for i in results]
@@ -132,6 +62,7 @@ class Cron:
 			deleteblank = re.match(r"^\S+",res.group())
 			if deleteblank is None:
 				raise CronRegularExpressionError("invalid cron string")
+			# *
 			before_slash = deleteblank.group().split('/')[0]
 
 			index = results.index(res)
@@ -150,7 +81,7 @@ class Cron:
 					"mode":list(CronScale)[index],
 				})
 				
-
+			# 10
 			if len(deleteblank.group().split('/')) > 1:
 				after_slash = deleteblank.group().split('/')[1]
 				self._intervals.append({
@@ -159,3 +90,58 @@ class Cron:
 					"mode":list(CronScale)[index],
 				})
 				
+	def _quote_replace(self):
+		return self._command.replace('"','\\"')
+
+	@property
+	def _list(self):
+		result = subprocess.Popen(["crontab","-l"],encoding="utf-8",stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		comm = result.communicate()
+		errout = comm[1]
+		stdout = comm[0]
+		if re.match(r'^no crontab',errout) is not None:
+			return None
+		else:
+			return stdout
+	def register(self):
+		if self._list is None:
+			input = self._totalline
+		else:
+			iter = re.finditer(r'.*\n',self._list)
+			cronlists = [x.group() for x in iter]
+			# delete duplication
+			cronlists = list(set(cronlists))
+			if self._totalline not in cronlists: 
+				cronlists.append(self._totalline)
+			input = "".join(list(set(cronlists)))	
+		res = subprocess.run(["crontab"],input=input,encoding='utf-8')
+		return res.returncode
+	@property
+	def list(self):
+		lists = self._list
+		if lists is None:
+			return None
+
+		iter = re.finditer(r'.*\n',lists)
+		cronlists = [x.group() for x in iter]
+		return cronlists
+	@property
+	def count(self):
+		return len(self.list if self.list is not None else list())
+	def delete(self):
+		if self._list is None:
+			return None
+		else:
+			iter = re.finditer(r'.*\n',self._list)
+			cronlists = [x.group() for x in iter]
+			# delete duplication
+			cronlists = list(set(cronlists))
+			if self._totalline in cronlists: 
+				cronlists.remove(self._totalline)
+			if len(cronlists) == 0:
+				res = subprocess.run(["crontab","-r"])
+				return res.returncode
+			else:
+				input = "".join(list(set(cronlists)))	
+				res = subprocess.run(["crontab"],input=input,encoding='utf-8')
+				return res.returncode
--- tests/test_cron.py
+++ tests/test_cron.py
@@ -1,5 +1,7 @@
 import unittest
+import unittest.mock
 from resc.cron import *
+import subprocess
 
 _INTERVAL=10
 _INTERVAL_MODE="interval"
@@ -7,31 +9,53 @@ _INTERVAL_SCALE="day"
 
 class TestCron(unittest.TestCase):
 	def test_cron(self):
-#		cron = Cron(
-#			command='echo "Hello world"',
-#			intervals=[{"interval":_INTERVAL,"scale":_INTERVAL_SCALE,"mode":_INTERVAL_MODE}]
-#		)
-#
-#		res = cron.intervals
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,list)
-#
-#		print(f'CRON INTERVAL LISTS {res}')
-
 		cron = Cron(
 			command='echo "Hello world"',
 			interval_str="*/1 0-3 * * *",
 		)
-		print(cron._intervals)
+		res = cron.interval_str
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,str)
+		print(f"CRON INTERVAL STRING \"{res}\"")
 
-#		res = cron.interval_str
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,str)
-#		print(f'CRON INTERVAL STRING {res}')
-#		res = cron.intervals
-#		self.assertIsNotNone(res)
-#		self.assertIsInstance(res,list)
-#		print(f'CRON INTERVAL LISTS {res}')
+		res = cron.totalline
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,str)
+		print(f"CRON TOTAL LINE \"{res}\"")
+
+		res = cron.register()
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,int)
+		print(f"CRON REGISTER END STATUS \"{res}\"")
+		res = cron.list
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,list)
+		print(f"CRON LISTS {res}")
+		res = cron.count
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,int)
+		print(f"CRON COUNT {res}")
+
+		res = cron.delete()
+		self.assertIsNotNone(res)
+		self.assertIsInstance(res,int)
+		print(f"CRON DELETE END STATUS \"{res}\"")
+	
+#	def test_register(self):
+#		cron = Cron(
+#			command='echo "Hello world"',
+#			interval_str="*/1 0-3 * * *",
+#		)
+#
+#		m = unittest.mock.MagicMock()
+#		subprocess.run = m
+#		cron.register()
+#		m.assert_called_with(["crontab"],input=f'{cron.totalline}\n',encoding="utf-8")
+#
+#		m = unittest.mock.MagicMock()
+#		subprocess.run = m
+#		cron.list()
+#		m.assert_called_with(["crontab","-l"],encoding="utf-8")
 
 if __name__ == "__main__":
 	unittest.main()
