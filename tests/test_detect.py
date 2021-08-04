b37ffba3d1123b81672ff9b4cb1df3cf308ea9f3
tatsuya <tatsuya4649developer@gmail.com>
2021-08-04 20:37:16 +0900

    resc/detect.py coverage test(100%)

---
+++ tests/test_detect.py
@@ -0,0 +1,45 @@
+import pytest
+from resc.detect import *
+
+@pytest.fixture(scope="function",autouse=False)
+def setup_dbase():
+    # Base Class of Resource Detect
+    detect_base = DetectBase()
+    yield detect_base
+
+
+def test_resource(setup_dbase):
+    with pytest.raises(
+        DetectBaseNotImplementedError
+    ) as raiseinfo:
+        setup_dbase.resource
+    print(f"DETECT BASE CLASS RESOURCE IMPL: {raiseinfo.value}")
+
+def test_mode(setup_dbase):
+    with pytest.raises(
+        DetectBaseNotImplementedError
+    ) as raiseinfo:
+        setup_dbase.mode
+    print(f"DETECT BASE CLASS MODE IMPL: {raiseinfo.value}")
+
+def test_check(setup_dbase):
+    with pytest.raises(
+        DetectBaseNotImplementedError
+    ) as raiseinfo:
+        setup_dbase.check
+    print(f"DETECT BASE CLASS CHECK IMPL: {raiseinfo.value}")
+
+def test_threshold(setup_dbase):
+    with pytest.raises(
+        DetectBaseNotImplementedError
+    ) as raiseinfo:
+        setup_dbase.threshold
+    print(f"DETECT BASE CLASS THRESHOLD IMPL: {raiseinfo.value}")
+
+def test_percent(setup_dbase):
+    with pytest.raises(
+        DetectBaseNotImplementedError
+    ) as raiseinfo:
+        setup_dbase.percent
+    print(f"DETECT BASE CLASS PERCENT IMPL: {raiseinfo.value}")
+
