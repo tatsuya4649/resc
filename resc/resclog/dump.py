ed295924f740961cf65eeb69f23b033cf56b4573
tatsuya <tatsuya4649developer@gmail.com>
2021-08-05 15:43:33 +0900

    resclog/dump.py coverage test added

--- resc/resclog/dump.py
+++ resc/resclog/dump.py
@@ -8,22 +8,36 @@ class RescDumpTypeError(TypeError):
 
 class RescDump:
     def __init__(self, content):
-        if not isinstance(content, bytes):
-            raise RescDumpTypeError(f"content({content}) \
-                must be bytes type.now {type(content)}.")
+        self._content_type(content)
         self._content = content
 
     @classmethod
-    def bindump(self, content):
+    def _content_type(self,content):
+        if not isinstance(content, bytes):
+            raise RescDumpTypeError(f"content({content}) \
+                must be bytes type.now {type(content)}.")
+    @classmethod
+    def bindump(
+        self,
+        content,
+        ascii=False,
+        empty=True,
+    ):
+        self._content_type(content)
         bitarray = bitstring.ConstBitArray(bytes=content)
         binary = bitarray.bin
         splits = re.split(r'(.{8})', binary)[1::2]
         for i in splits:
             bytebit = bitstring.ConstBitArray(f"0b{i}")
-            splits[splits.index(i)] = f"{i}({bytebit.bytes})"
-        return " ".join(splits)
+            if ascii:
+                splits[splits.index(i)] = f"{i}({bytebit.bytes})"
+            else:
+                splits[splits.index(i)] = f"{i}"
+        gap_str = " " if empty else ""
+        return gap_str.join(splits)
 
     @classmethod
     def hexdump(self, content):
+        self._content_type(content)
         bitarray = bitstring.ConstBitArray(bytes=content)
         return bitarray.hex
