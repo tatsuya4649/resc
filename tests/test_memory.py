6538cfc03973aba4a4ce93592a9cd80e066e99b2
tatsuya <tatsuya4649developer@gmail.com>
2021-08-06 12:06:59 +0900

    memory.py coverage test 100%

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,3 +1,5 @@
+import pytest
+from unittest import mock
 from resc.memory import *
 
 def test_memory():
@@ -27,3 +29,55 @@ def test_memory():
 	assert res is not None
 	assert isinstance(res,bool)
 	print(f'MEMORY CHECK {res} (type(res))')
+
+@pytest.mark.parametrize(
+    "threshold",[
+    "1",
+    "1.0",
+    [1],
+    {"threshold": 1}
+])
+def test_memory_init_type_error(threshold):
+    with pytest.raises(
+        MemoryTypeError
+    ) as raiseinfo:
+        memory = MemoryDetect(threshold)
+
+@pytest.mark.parametrize(
+    "mode",[
+    1,
+    1.0,
+    ["percent"],
+    {"mode": "percent"}
+])
+def test_memory_init_mode_type_error(mode):
+    with pytest.raises(
+        MemoryTypeError
+    ) as raiseinfo:
+        memory = MemoryDetect(
+            threshold=80.0,
+            mode=mode
+        )
+
+def test_memory_check_false():
+    with mock.patch(
+        "resc.memory.MemoryDetect.percent",
+        return_value = 90.0
+    ):
+        memory = MemoryDetect(
+            threshold=80.0
+        )
+        assert memory.check is False
+
+def test_memory_check_none():
+    with mock.patch(
+        "resc.memory.MemoryDetect.percent",
+        return_value = None
+    ):
+        memory = MemoryDetect(
+            threshold=80.0
+        )
+        with pytest.raises(
+            MemoryValueError
+        ) as raiseinfo:
+            memory.check
